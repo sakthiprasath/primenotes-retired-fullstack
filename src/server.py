4a65ac94195e3d81554e34d3a5069b4a6da0a05a
sakthi <sakthi@symphonyai.com>
2021-04-19 21:26:22 +0530

    fixing merge conflicts

--- src/server.py
+++ src/server.py
@@ -1,33 +1,22 @@
 #! /usr/bin/env python3
 import logging
 from flask import Flask
-
 from flask_rest_api import Api
-<<<<<<< HEAD
-import traceback
-from http.client import HTTPException
-=======
->>>>>>> 594cf3632af2fc50cf74d12b36b1781e2dbaaea5
 from src.rest.routes.individual_component_fetcher import individual_component_fetcher_routes
 from src.rest.routes.tree_note_routes import tree_note_routes
 
 
 flask_logger = logging.getLogger('werkzeug')
-
 app = Flask(__name__)
-
-
 app.config['OPENAPI_VERSION'] = '3.0.2'
 api = Api(app)
 
 api.register_blueprint(individual_component_fetcher_routes)
 api.register_blueprint(tree_note_routes)
 
-
 @app.before_request
 def validate_access_token():
     pass
-
 
 #@app.before_request
 def before():
@@ -40,8 +29,6 @@ def before():
 @app.errorhandler(Exception)
 def handle_error(e):
     pass
-
-
 
 
 if __name__ == '__main__':
